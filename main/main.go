53dbf3a255e3bd56ee5cded9f5f2e0f8022b1b7d
TheGitHubist <131190563+TheGitHubist@users.noreply.github.com>
2024-04-08 13:42:08 +0200

    adding basics (a handler, an html, a css and password checker)

--- main/main.go
+++ main/main.go
@@ -1,5 +1,24 @@
 package main
 
+import (
+	"html/template"
+	"net/http"
+)
+
+func UserPageHandler(w http.ResponseWriter, r *http.Request) {
+	templ, err := template.ParseFiles("../templates/user.html")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	err = templ.Execute(w, nil)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
 func main() {
 
 }
