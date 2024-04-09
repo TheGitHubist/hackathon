721e4dfe9acf77c9484964d454e9cbc7ada21b22
chhugo <hugo.chamand@ynov.com>
2024-04-09 11:18:12 +0200

    adding handlers

--- main/main.go
+++ main/main.go
@@ -6,8 +6,25 @@ import (
 	"net/http"
 )
 
-func UserPageHandler(w http.ResponseWriter, r *http.Request) {
-	templ, err := template.ParseFiles("../templates/user.html")
+type PageData struct {
+	Title   string
+	Message string
+}
+
+func main() {
+	http.HandleFunc("/home", UserPageHandler)
+	http.HandleFunc("/admin", adminHandler)
+	http.HandleFunc("/backend", backHandler)
+	fmt.Println("le serveur est lancer sur le port 8080")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		panic(err)
+	}
+	fs := http.FileServer(http.Dir("../styles"))
+	http.Handle("/styles/", http.StripPrefix("/styles/", fs))
+}
+
+func adminHandler(w http.ResponseWriter, r *http.Request) {
+	templ, err := template.ParseFiles("../templates/admin.html")
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
@@ -20,12 +37,29 @@ func UserPageHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func main() {
-	http.HandleFunc("/home", UserPageHandler)
-	fmt.Println("le serveur est lancer sur le port 8080")
-	if err := http.ListenAndServe(":8080", nil); err != nil {
-		panic(err)
+func backHandler(w http.ResponseWriter, r *http.Request) {
+	templ, err := template.ParseFiles("../templates/back.html")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
-	fs := http.FileServer(http.Dir("../styles"))
-	http.Handle("/styles/", http.StripPrefix("/styles/", fs))
+
+	err = templ.Execute(w, nil)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+func UserPageHandler(w http.ResponseWriter, r *http.Request) {
+	templ, err := template.ParseFiles("../templates/user.html")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	err = templ.Execute(w, nil)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 }
