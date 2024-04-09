d82478eca90619bd3d6c639694706cfda5afc149
TheGitHubist <131190563+TheGitHubist@users.noreply.github.com>
2024-04-09 14:28:14 +0200

    finalizing hash

--- controller/Crypt.go
+++ controller/Crypt.go
@@ -1,5 +1,11 @@
 package hackathon
 
+import (
+	"fmt"
+	"math/rand"
+	"strconv"
+)
+
 func convStringByte(sentence string) []byte {
 	byteArray := []byte(sentence)
 	return byteArray
@@ -62,34 +68,82 @@ func invertString(s string) string {
 	return newS
 }
 
-func turnHexa(n int) string {
-	hexaChar := "0123456789abcdef"
-	base := ""
-	for n > 15 {
-		base += string(hexaChar[n%16])
-		n -= n % 16
-		n /= 16
+func power(n int, p int) int {
+	pow := 1
+	for i := 0; i < p; i++ {
+		pow *= n
 	}
-	base += string(hexaChar[n%16])
-	return invertString(base)
+	return pow
 }
 
 func crypt(sentence string, ranNumber int) string {
 	ByteTable := convStringByte(sentence)
 	outsideTable := make([]byte, len(sentence))
 	finalTable := ByteTable
-	finalTable = nand(xor(not(xor(ByteTable, outsideTable)), not(or(and(ByteTable, outsideTable), not(and(ByteTable, outsideTable))))), not(xor(not(and(ByteTable,
-		outsideTable)), or(ByteTable, outsideTable))))
+	finalTable = nand(xor(not(xor(ByteTable, outsideTable)), not(or(and(ByteTable, outsideTable), not(and(ByteTable, outsideTable))))), not(xor(not(and(ByteTable, outsideTable)), or(ByteTable, outsideTable))))
 	finalValue := totalValueSum(finalTable)
-	return turnHexa(finalValue * (len(finalTable) * ranNumber))
+	return strconv.FormatInt(int64(finalValue*(len(finalTable)*power(ranNumber, 4))), 16)
 }
 
-func FinalCrypt(sentence string, salt int) string {
+func FinalCrypt(sentence string) string {
 	inv := invertString(sentence)
 	cr := ""
+	ran := rand.Intn(999999999-999999) + 999999
+	fmt.Println(ran)
 	for i := 0; i < len(sentence); i++ {
-		cr += crypt(string(sentence[i]), salt)
-		cr += crypt(string(inv[i]), salt)
+		cr += crypt(string(sentence[i]), ran)
+		cr += crypt(string(inv[i]), ran)
 	}
-	return cr
+	return gatherHexaToOne(cr)
+}
+
+func averageHexa(hexa1 string, hexa2 string) string {
+	hexaChar := "0123456789abcdef"
+	newHexa := ""
+	for i, c := range hexa1 {
+		ch1 := 0
+		ch2 := 0
+		for j := 0; j < len(hexaChar); j++ {
+			if hexaChar[j] == byte(c) {
+				ch1 = j
+			} else if hexaChar[j] == hexa2[i] {
+				ch2 = j
+			}
+		}
+		newHexa += strconv.FormatInt(int64((ch1+ch2)/2), 16)
+	}
+	return newHexa
+}
+
+func gatherHexaToOne(hexa string) string {
+	table := []string{}
+	sign := ""
+	for i, c := range hexa {
+		sign += string(c)
+		if len(sign) == 64 {
+			table = append(table, sign)
+			sign = ""
+		} else if i == len(hexa)-1 {
+			table = append(table, sign)
+			sign = ""
+		}
+	}
+	newHexa := ""
+	if len(table) == 1 {
+		return table[0]
+	}
+	for len(table) > 0 {
+		if newHexa == "" {
+			newHexa += averageHexa(table[len(table)-1], table[len(table)-2])
+			table = table[:len(table)-1]
+			table = table[:len(table)-1]
+		} else {
+			if len(table[len(table)-1]) < 64 {
+				break
+			}
+			newHexa += averageHexa(newHexa, table[len(table)-1])
+			table = table[:len(table)-1]
+		}
+	}
+	return newHexa
 }
